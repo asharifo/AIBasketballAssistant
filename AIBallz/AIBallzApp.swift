b8cab9fca2b7fe8037d49ae8edba20f73c81c063
Amir Sharifov <asharifo@students.cs.ubc.ca>
2025-08-06 22:17:48 +0500

    Initial Commit

---
+++ AIBallz/AIBallzApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBallzApp.swift
+//  AIBallz
+//
+//  Created by Amir Sharifov on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct AIBallzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIBallzTests/AIBallzTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIBallzTests.swift
+//  AIBallzTests
+//
+//  Created by Amir Sharifov on 2025-08-06.
+//
+
+import Testing
+@testable import AIBallz
+
+struct AIBallzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
